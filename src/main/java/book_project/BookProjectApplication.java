20b1d66c469e3045f58f4b8d1b711a3c6421d41c
Marema Gemechis <a1504540@myy.haaga-helia.fi>
2017-03-04 23:08:58 +0200

    first commit

---
+++ src/main/java/book_project/BookProjectApplication.java
@@ -0,0 +1,12 @@
+package book_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookProjectApplication.class, args);
+	}
+}
